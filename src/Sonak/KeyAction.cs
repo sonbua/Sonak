6cf1661ead0c9d48fa84557cc8a4aef70891fc58
Son Nguyen <dinh.son.nguyen@gmail.com>
2016-11-17 19:33:29 +0700

    Add equality testing of two key actions.

--- src/Sonak/KeyAction.cs
+++ src/Sonak/KeyAction.cs
@@ -18,5 +18,33 @@
         /// The key, which is being in interaction.
         /// </summary>
         public Key Key { get; }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != GetType())
+                return false;
+            return Equals((KeyAction) obj);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return Key?.GetHashCode() ?? 0;
+        }
+
+        /// <summary>
+        /// Tests the equality of this instance to other instance.
+        /// </summary>
+        /// <param name="other">The other object in equality test.</param>
+        /// <returns></returns>
+        protected bool Equals(KeyAction other)
+        {
+            return Equals(Key, other.Key);
+        }
     }
 }
